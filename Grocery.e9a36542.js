4ede266ad402f1f0fc7d82b4a63f8cbd0f9106ce
Karthik <bankapallikarthik03@gmail.com>
2025-04-02 01:10:12 +0530

    Updates

---
+++ Grocery.e9a36542.js
@@ -0,0 +1,2 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire94c2;(0,e.register)("6okWW",function(o,r){Object.defineProperty(o.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(o.exports,"default",{get:function(){return i},set:void 0,enumerable:!0,configurable:!0});var n=e("jk232"),i=()=>(0,n.jsx)("div",{className:"GroceryPage",children:(0,n.jsx)("h1",{children:" Welcome to Swiggy Grocery Home Page"})})});
+//# sourceMappingURL=Grocery.e9a36542.js.map
